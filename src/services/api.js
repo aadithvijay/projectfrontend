a4e2eac1dd085c96d95b77b71662c13f6a47d186
aadith vijay <aadithvijay7@gmail.com>
2026-05-31 18:28:49 +0530

    last commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://projectbackend-wupg.onrender.com",
+  baseURL: "https://projectbackend-wupg.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
